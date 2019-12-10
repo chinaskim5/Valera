9c44047393de8859d06a2793e0afb7f70d8b7363
vvsmrl <kolokolnikovkm@gmail.com>
2019-12-10 12:42:18 +0700

    Fix select_action

--- lib/states/select_action.rb
+++ lib/states/select_action.rb
@@ -1,15 +1,27 @@
 # frozen_string_literal: true
 
-require_relative './base'
-require_relative './player_state'
+require_relative './save_player'
 
 module GameStates
-  class ResetPlayer < Base
-    def render; end
+  class SelectAction < Base
+    def render
+      puts 'Select an action:'
+      context[:actions].each_with_index { |action, id| puts "#{id + 1}) #{action.name}" }
+      puts "#{context[:actions].length + 1}) Quit the game"
+    end
 
     def next
-      context[:valera] = Valera.new
-      SavePlayer.new context
+      action_id = gets.to_i - 1
+      case action_id
+      when 0...context[:actions].length
+        context[:actions][action_id].run context[:valera]
+        SavePlayer.new context
+      when context[:actions].length
+        LeaveGame.new
+      else
+        puts 'Invalid action!'
+        self
+      end
     end
   end
 end
